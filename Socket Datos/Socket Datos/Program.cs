189f7600485074ac34d3542052a8167f9ab039d5
Cotue <148938165+Cotue@users.noreply.github.com>
2024-07-30 14:54:29 -0600

    Add files via upload

---
+++ Socket Datos/Socket Datos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Timers;
+using System.Threading;
+namespace SocketMessage
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread serverThread = new Thread(new ThreadStart(StartServer));
+            serverThread.Start();
+
+            // Delay to ensure the server starts first
+            Thread.Sleep(1000);
+
+            Client client = new Client();
+            client.client();
+        }
+
+        static void StartServer()
+        {
+            Server server = new Server();
+            server.server();
+        }
+
+        
+    }
+}
